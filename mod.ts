0ba003ef33fa8b0a59d71372b0717569a35f0ad7
Gnlow <gnlow@pm.me>
2025-03-21 16:28:11 +0900

    messed up

---
+++ mod.ts
@@ -0,0 +1,30 @@
+type F = (...xs: number[]) => number
+
+const nr =
+(
+    f: F,
+    dfs: F[],
+    x0: number[],
+    tol = 1e-6,
+    maxIter = 100,
+) => {
+    let x = [...x0]
+
+    for (let iter=0; iter<maxIter; iter++) {
+        const fx = f(...x)
+        const grad = dfs.map(df => df(...x))
+
+        const diff = Math.hypot(...grad)
+        console.log(diff)
+        if (diff < tol) return x
+
+        x = x.map((x, i) => x - grad[i])
+    }
+    return x
+}
+
+console.log(nr(
+    x => x**2-2,
+    [x => 2*x],
+    [2],
+))
