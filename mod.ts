cbe15643fc29e8aacef42e4bf2ab68f7b4932101
Gnlow <gnlow@pm.me>
2025-03-21 17:21:04 +0900

    umm
    in 2d, work on some point but other

--- mod.ts
+++ mod.ts
@@ -6,18 +6,19 @@ const nr =
     dfs: F[],
     x0: number[],
     tol = 1e-6,
-    maxIter = 100,
+    maxIter = 10,
 ) => {
     let x = [...x0]
 
     for (let iter=0; iter<maxIter; iter++) {
         const fx = f(...x)
+        console.log(x, "->", fx)
+        if (Math.abs(fx) < tol) {
+            console.log(iter)
+            return x
+        }
+
         const grad = dfs.map(df => df(...x))
-
-        const diff = Math.hypot(...grad)
-        console.log(diff)
-        if (diff < tol) return x
-
         x = x.map((x, i) => x - fx / grad[i])
     }
     return x
@@ -26,5 +27,11 @@ const nr =
 console.log(nr(
     x => x**2-2,
     [x => 2*x],
-    [2],
+    [-1],
 )) // [ 1.414 ]
+
+console.log(nr(
+    (x, y) => (x-3)**2+(y-4)**2,
+    [(_x, y) => 2*y-8, (x, _y) => 2*x-6],
+    [5, 5],
+))
